ff4ac9a86aafd1054128931adb0955674cdd4e37
Michael Nnaji <mnnaji3@uwo.ca>
2020-08-05 05:53:24 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -6,7 +6,7 @@ attr_accessor :player_1, :player_2, :board
  WIN_COMBINATIONS = [ [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
 
 
-def initialize(player_1=("X"),player_2=("O"),board=Board.new)
+def initialize(player_1 = Players::Human.new("X"),player_2 = Players::Human.new("O"),board=Board.new)
   @player_1 = player_1
   @player_2 = player_2
   @board = board
@@ -158,24 +158,7 @@ def play
 
 
 
-def turn
- puts "Please choose a number 1-9:"
-  user_input = gets
-  index = self.board.input_to_index(user_input)
-  until self.over? === true
-  if self.board.valid_move?(index)
-    player_token = current_player.token
-    move(index, player_token)
-    self.board.display
-  else
-    turn
-    if self.board.full? || self.game.won?
-      break
-    end
-  end
-  end
 
-end
 
 
 end
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,7 @@ module Players
 class Computer < Player
   def move(board)
     input = rand 1..9
-    return input
+    return input.to_s
   end
 
 
